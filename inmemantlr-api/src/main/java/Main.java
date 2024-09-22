936fdcf58a72baa8fc66f61dde752f216c986778
Remme, Lian (lirem101) <lirem101@hhu.de>
2024-09-22 10:12:20 +0200

    Added Main

---
+++ inmemantlr-api/src/main/java/Main.java
@@ -0,0 +1,42 @@
+import org.snt.inmemantlr.GenericParser;
+import org.snt.inmemantlr.exceptions.CompilationException;
+import org.snt.inmemantlr.exceptions.IllegalWorkflowException;
+import org.snt.inmemantlr.exceptions.ParsingException;
+import org.snt.inmemantlr.listener.DefaultTreeListener;
+import org.snt.inmemantlr.tree.ParseTree;
+import org.snt.inmemantlr.utils.FileUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static final String resourcePath = System.getProperty("user.dir") + "/inmemantlr-api/src/main/resources/";
+
+    public static void main(String[] args) throws FileNotFoundException, CompilationException, ParsingException, IllegalWorkflowException {
+        System.out.println(System.getProperty("user.dir"));
+
+        File f = new File(resourcePath + "Quil.g4");
+        GenericParser gp = new GenericParser(f);
+        // 2. load file content into string
+        String s = FileUtils.loadFileContent(resourcePath + "teleport.quil");
+        // 3. set listener for checking parse tree elements. Here you could use any ParseTreeListener implementation. The default listener is used per default
+        // this listener will create a parse tree from the java file
+        DefaultTreeListener dlist = new DefaultTreeListener();
+        gp.setListener(dlist);
+        // 4. compile Lexer and parser in-memory
+        gp.compile();
+        // 5. parse the string
+        gp.parse(s, null, GenericParser.CaseSensitiveType.NONE);
+
+        ParseTree pt = dlist.getParseTree();
+
+        String xml = pt.toXml();
+        String json = pt.toJson();
+        String dot = pt.toDot();
+
+        System.out.println(xml);
+        System.out.println(json);
+        System.out.println(dot);
+
+    }
+}
