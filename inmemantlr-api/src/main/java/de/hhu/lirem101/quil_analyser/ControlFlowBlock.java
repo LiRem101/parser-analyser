e5591f17c2c55a2c372be788f4bbf81ee56b696c
Remme, Lian (lirem101) <lirem101@hhu.de>
2024-09-28 19:18:33 +0200

    Added name

--- inmemantlr-api/src/main/java/de/hhu/lirem101/quil_analyser/ControlFlowBlock.java
+++ inmemantlr-api/src/main/java/de/hhu/lirem101/quil_analyser/ControlFlowBlock.java
@@ -6,10 +6,16 @@ import java.util.ArrayList;
  * Represents a control flow block that contains code lines and branches.
  */
 public class ControlFlowBlock {
+    // Start label/name of the block.
+    private final String name;
     // List of code lines in this control flow block, in the order that they are being executed.
     private final ArrayList<Integer> codelines = new ArrayList<>();
     // Pointers to the next control flow blocks.
     private final ArrayList<ControlFlowBlock> branches = new ArrayList<>();
+
+    public ControlFlowBlock(String name) {
+        this.name = name;
+    }
 
     public void addCodeline(int codeline) {
         codelines.add(codeline);
@@ -25,5 +31,9 @@ public class ControlFlowBlock {
 
     public ArrayList<ControlFlowBlock> getBranches() {
         return new ArrayList<>(branches);
+    }
+
+    public String getName() {
+        return name;
     }
 }
