0cac7a4a065cf037abb1f77d2deee6acddc2be63
Remme, Lian (lirem101) <lirem101@hhu.de>
2024-11-05 08:14:48 +0100

    Optimizing Quil main class

---
+++ inmemantlr-api/src/main/java/de/hhu/lirem101/quil_optimizer/OptimizingQuil.java
@@ -0,0 +1,28 @@
+package de.hhu.lirem101.quil_optimizer;
+
+import de.hhu.lirem101.quil_analyser.ControlFlowBlock;
+import de.hhu.lirem101.quil_analyser.LineType;
+import org.snt.inmemantlr.tree.ParseTreeNode;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class OptimizingQuil {
+    private final ArrayList<ArrayList<InstructionNode>> instructions;
+
+
+    /**
+     * Constructor for the OptimizingQuil class. Creates a list of list of instructions from a control flow block.
+     * @param block The control flow block to create the instructions from.
+     * @param classes A map with the line number as key and the line type as value.
+     * @param root The root node of the parse tree.
+     */
+    public OptimizingQuil(ControlFlowBlock block, Map<Integer, LineType> classes, ParseTreeNode root) {
+        InstructionListCreator ilc = new InstructionListCreator(block, classes);
+        SortingNodesToLines snl = new SortingNodesToLines(root);
+        Map<Integer, ParseTreeNode> sortedNodes = snl.getSortedNodes();
+        SortNodesIntoInstructions sorter = new SortNodesIntoInstructions(sortedNodes);
+        this.instructions = ilc.getInstructions();
+        sorter.appendNodeToInstructions(instructions);
+    }
+}
