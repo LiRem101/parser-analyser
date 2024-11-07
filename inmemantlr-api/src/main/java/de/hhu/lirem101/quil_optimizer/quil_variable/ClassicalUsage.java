95d36f101bcf92b340583fcf3307fd35bf676e53
Remme, Lian (lirem101) <lirem101@hhu.de>
2024-11-07 15:20:22 +0100

    Add usage_assignment

--- inmemantlr-api/src/main/java/de/hhu/lirem101/quil_optimizer/quil_variable/ClassicalUsage.java
+++ inmemantlr-api/src/main/java/de/hhu/lirem101/quil_optimizer/quil_variable/ClassicalUsage.java
@@ -3,5 +3,5 @@ package de.hhu.lirem101.quil_optimizer.quil_variable;
 import javax.accessibility.AccessibleStateSet;
 
 public enum ClassicalUsage {
-    DECLARE, USAGE, ASSIGNMENT
+    DECLARE, USAGE, ASSIGNMENT, USAGE_ASSIGNMENT;
 }
