6c14a40011f94d04e5c885275176f30d3d87c421
Remme, Lian (lirem101) <lirem101@hhu.de>
2024-11-08 10:15:17 +0100

    Added RelevantNodeRules

--- inmemantlr-api/src/main/java/de/hhu/lirem101/quil_optimizer/quil_variable/RelevantNodeRules.java
+++ inmemantlr-api/src/main/java/de/hhu/lirem101/quil_optimizer/quil_variable/RelevantNodeRules.java
@@ -1,11 +1,21 @@
 package de.hhu.lirem101.quil_optimizer.quil_variable;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class RelevantNodeRules {
-    // Gates that help deciding on the usage type of a variable
-    public static Set<String> gateNames = new HashSet<>(Arrays.asList("NEG", "NOT", "TRUE", "FALSE", "logicalBinaryOp",
-            "arithmeticBinaryOp", "move", "exchange", "convert", "classicalComparison"));
+    // Gates that help deciding on the usage type of a classical variable
+    public static HashMap<String, List<ClassicalUsage>> gateUsage() {
+        HashMap<String, List<ClassicalUsage>> gateUsage = new HashMap<>();
+        gateUsage.put("NEG", Collections.singletonList(ClassicalUsage.USAGE_ASSIGNMENT));
+        gateUsage.put("NOT", Collections.singletonList(ClassicalUsage.USAGE_ASSIGNMENT));
+        gateUsage.put("TRUE", Collections.singletonList(ClassicalUsage.ASSIGNMENT));
+        gateUsage.put("FALSE", Collections.singletonList(ClassicalUsage.ASSIGNMENT));
+        gateUsage.put("logicalBinaryOp", Arrays.asList(ClassicalUsage.USAGE_ASSIGNMENT, ClassicalUsage.USAGE));
+        gateUsage.put("arithmeticBinaryOp", Arrays.asList(ClassicalUsage.USAGE_ASSIGNMENT, ClassicalUsage.USAGE));
+        gateUsage.put("move", Arrays.asList(ClassicalUsage.ASSIGNMENT, ClassicalUsage.USAGE));
+        gateUsage.put("exchange", Arrays.asList(ClassicalUsage.USAGE_ASSIGNMENT, ClassicalUsage.USAGE_ASSIGNMENT));
+        gateUsage.put("convert", Arrays.asList(ClassicalUsage.ASSIGNMENT, ClassicalUsage.USAGE));
+        gateUsage.put("classicalComparison", Arrays.asList(ClassicalUsage.ASSIGNMENT, ClassicalUsage.USAGE, ClassicalUsage.USAGE));
+        return gateUsage;
+    }
 }
